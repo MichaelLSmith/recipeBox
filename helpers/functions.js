0c0032c061685b11c344f05f717f7cbb8b06a65b
MichaelLSmith <mlsmith45@gmail.com>
2017-05-18 15:15:31 -0400

    Updates

--- helpers/functions.js
+++
@@ -1,6 +0,0 @@
-export function stringToArray(str) {
-  //this will take in a comma delimited string and convert it to an array
-  let arr = _.split(str, ',');
-  arr = arr.map(v => _.trim(v));
-  return arr;
-}
